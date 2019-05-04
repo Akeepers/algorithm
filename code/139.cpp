92152eeb732cc3087f5f3b5ae4248a95070c27e3
Akeepers <578805584@qq.com>
2019-05-04 18:38:31 +0800

    add solutions

---
+++ code/139.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        unordered_set<string> dicts;
+        for (auto item : wordDict)
+        {
+            dicts.insert(item);
+        }
+        vector<int> dp(s.size(), false);
+        if (dicts.find(to_string(s[0])) != dicts.end())
+            dp[0] = true;
+        for (int i = 1; i < s.size(); ++i)
+        {
+            auto word = s.substr(0, i + 1);
+            if (dicts.find(word) != dicts.end())
+            {
+                dp[i] = true;
+                continue;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (dp[j])
+                {
+                    word = s.substr(j, i - j);
+                    if (dicts.find(word) != dicts.end())
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.size() - 1];
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<string> inputs={"leet","code"};
+    solution.wordBreak("leetcode",inputs);
+    system("pause");
+    return 0;
+}
