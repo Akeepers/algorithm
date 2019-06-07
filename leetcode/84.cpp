9d5876715f3357542cfdaf358dc89102823fba01
akeeper <578805584@qq.com>
2019-06-07 18:07:49 +0800

    add solution

--- leetcode/84.cpp
+++ leetcode/84.cpp
@@ -13,12 +13,18 @@ public:
         if (heights.size() == 1)
             return heights[0];
         int n = heights.size();
-        int minHeight = heights[0];
+        int minHeight = heights[0], count = 1;
         vector<int> dp(n, 0);
         dp[0] = heights[0];
         for (int i = 1; i < n; ++i)
         {
-            
+            if (minHeight <= heights[i])
+            {
+                dp[i] = dp[i - 1] + minHeight;
+                if(dp[i<heights[i])
+                {
+                }
+            }
         }
     }
 };
