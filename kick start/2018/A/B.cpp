15d65969c9d6e0c7168657bccb9977de25b6ba68
Akeepers <578805584@qq.com>
2019-07-05 19:58:01 +0800

    add solution

--- kick start/2018/A/B.cpp
+++ kick start/2018/A/B.cpp
@@ -1,25 +1,31 @@
 #include <algorithm>
 #include <climits>
+#include <iomanip>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 using namespace std;
 
-double cal(int n, int k, vector<int> &values)
+double cal(int n, int k, vector<long long> &values)
 {
     sort(values.begin(), values.end());
-    vector<double> dp(k+1, 0);
-    long long sum = 0;
-    for (auto value : values)
-        sum += value;
-    dp[0] = sum / (double)n;
+    vector<double> dp(k + 1, 0);
+    vector<long long> sum(n + 1, 0);
+    sum[0] = values[0];
+    for (int i = n - 1; i >= 0; --i)
+    {
+        sum[i] = sum[i + 1] + values[i];
+    }
+    dp[0] = sum[0] / (double)n;
     if (k == 0)
         return dp[0];
-    double cur = 0;
-    for (int i = 1; i <= n;++i){
-        upper_bound(values.begin(),values.end(),dp[i-1])
+    for (int i = 1; i <= k; ++i)
+    {
+        int pos = distance(values.begin(), upper_bound(values.begin(), values.end(), dp[i - 1]));
+        dp[i] = (pos * dp[i - 1] + sum[pos]) / (double)n;
     }
-        return dp[k];
+    return dp[k];
 }
 
 int main()
@@ -30,12 +36,12 @@ int main()
     {
         long long n, k;
         cin >> n >> k;
-        vector<int> values(n, 0);
+        vector<long long> values(n, 0);
         for (int j = 0; j < n;)
             cin >> values[j++];
 
-        auto ret = cal(n,k,values);
-        cout<< "Case #" << i << ": " << ret << endl;
+        auto ret = cal(n, k, values);
+        cout << "Case #" << i << ": " << setiosflags(ios::fixed) << setprecision(6) << ret << endl;
     }
     return 0;
 }
