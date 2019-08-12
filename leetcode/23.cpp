8f72cc44598b42627eaed44e1e57f8dad56a0c25
Akeepers <578805584@qq.com>
2019-08-12 13:03:51 +0800

    add solution

--- leetcode/23.cpp
+++ leetcode/23.cpp
@@ -14,51 +14,107 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution
+bool mycompare(const ListNode *a, const ListNode *b)
+{
+    return a->val > b->val;
+}
+
+//minHeap
+class Solution1
 {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists)
     {
-        ListNode *head = new ListNode(0);
-        ListNode *max = new ListNode(INT_MAX);
-        ListNode *min = max;
-        vector<bool> visited(lists.size(), false);
-        int flag = 0;
-        ListNode *cur = cur;
-        while (flag < lists.size())
+        auto head = new ListNode(0);
+        auto node = head;
+        vector<ListNode *> minHeap;
+        for (auto item : lists)
         {
-            for (int i = 0; i < lists.size(); ++i)
+            if (item != nullptr)
+                minHeap.emplace_back(item);
+        }
+        make_heap(minHeap.begin(), minHeap.end(), mycompare);
+        while (!minHeap.empty())
+        {
+            node->next = minHeap.front();
+            node = node->next;
+            auto next = minHeap.front()->next;
+            node->next = nullptr;
+            pop_heap(minHeap.begin(), minHeap.end(), mycompare);
+            minHeap.pop_back();
+            if (next != nullptr)
             {
-                if (visited[i])
-                    continue;
-                if (lists[i] == nullptr)
-                {
-                    visited[i] = true;
-                    flag++;
-                    if (flag == lists.size())
-                        break;
-                    continue;
-                }
-                if (lists[i]->val < min->val)
-                {
-                    min = lists[i];
-                    cur = lists[i];
-                }
-              
-                head->next = min;
-                head = head->next;
-                cur = cur->next;
-                head->next = nullptr;
+                minHeap.push_back(next);
+                push_heap(minHeap.begin(), minHeap.end(), mycompare);
             }
-            min = max;
-            cur = max;
         }
         return head->next;
     }
 };
 
+//divide and conquer
+class Solution2
+{
+    private:
+    ListNode* mergeTwoLists(ListNode* root1,ListNode* root2){
+        if(root1==nullptr||root2==nullptr)
+            return root1 == nullptr ? root2 : root1;
+        auto root = new ListNode(0);
+        auto node = root;
+        while (root1 != nullptr && root2 != nullptr)
+        {
+            if(root1->val<root2->val){
+                node->next = root1;
+                node = node->next;
+                root1 = root1->next;
+            }else{
+                node->next = root2;
+                node=node->next;
+                root2 = root2->next;
+            }
+        }
+        node->next = root1 == nullptr ? root2 : root1;
+        return root->next;
+    }
+
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        ListNode* head = nullptr;
+        for(auto item:lists){
+            head = mergeTwoLists(head, item);
+        }
+        return head;
+    }
+};
+
+
+
 int main()
 {
+    ListNode *node1 = new ListNode(1);
+    auto node = node1;
+    node->next = new ListNode(4);
+    node = node->next;
+    node->next = new ListNode(5);
+    node = node->next;
+
+    ListNode *node2 = new ListNode(1);
+    node = node2;
+    node->next = new ListNode(3);
+    node = node->next;
+    node->next = new ListNode(4);
+    node = node->next;
+
+    ListNode *node3 = new ListNode(2);
+     node = node3;
+    node->next = new ListNode(6);
+    node = node->next;
+
+    vector<ListNode *> inputs = {node1, node2, node3};
+
+    Solution solution;
+    solution.mergeKLists(inputs);
     system("pause");
     return 0;
 }
