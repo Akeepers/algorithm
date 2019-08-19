7f6aa60135e2fa13deca363faad0f53cafce9489
akeeper <578805584@qq.com>
2019-08-19 13:10:50 +0800

    add solution

---
+++ leetcode/349.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+//hash table
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_set<int> keys(nums1.begin(),nums1.end());
+        vector<int> res;
+        for (auto num : nums2)
+        {
+            if (keys.count(num))
+            {
+                res.emplace_back(num);
+                keys.erase(num);
+            }
+        }
+        return res;
+    }
+};
+
+//two pointer & sort
+class Solution1
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> res;
+        int i=0,j=0;
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i]>nums2[j]){
+                i++;
+            }else if(nums1[i]<nums2[j]){
+                j++;
+            }else{
+                if(res.empty()||nums1[i]!=res.back())
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    system("pause");
+    return 0;
+}
---
+++ leetcode/350.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    system("pause");
+    return 0;
+}
