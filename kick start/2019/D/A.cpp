5a0296d5af4d0e5c78c4ccdc44de9ecc7a4b58a5
Akeepers <578805584@qq.com>
2019-09-02 16:18:16 +0800

    update solution

--- kick start/2019/D/A.cpp
+++ kick start/2019/D/A.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <bitset>
 #include <climits>
 #include <iostream>
 #include <set>
@@ -10,48 +11,46 @@ using namespace std;
 
 typedef long long ll;
 
-int bitCount(unsigned int n)
-{
-	unsigned int tmp = n - ((n >> 1) & 033333333333) - ((n >> 2) & 011111111111);
-	return ((tmp + (tmp >> 3)) & 030707070707) % 63;
-}
-
-int calMax(vector<int> &nums)
-{
-	int res = 0, left = 0, right = 0, maxSum = 0, curSum = 0, cur = 0;
-	for (int i = 0; i < nums.size(); ++i)
-	{
-		cur ^= nums[i];
-		curSum = bitCount(cur);
-		if (curSum > maxSum)
-		{
-			maxSum = curSum;
-		}
-	}
-}
-int slove(vector<int> &inputs, vector<pair<int, int>> &modifications)
-{
-}
-
 int main()
 {
+	vector<bool> xorOdd(1025);
+	for (int i = 0; i <= 1024; ++i)
+	{
+		xorOdd[i] = bitset<11>(i).count() & 1;
+	}
 	auto t = 0;
 	cin >> t;
 	for (int i = 1; i <= t; ++i)
 	{
-		int n, q, tmp1, tmp2;
+		cout << "Case #" << i << ":";
+		int n, q;
 		cin >> n >> q;
-		vector<int> inputs(n);
-		vector<pair<int, int>> modifications;
+		set<int> indexs;
 		for (int j = 0; j < n; j++)
-			cin >> inputs[j];
+		{
+			int x;
+			cin >> x;
+			if (xorOdd[x])
+				indexs.insert(j);
+		}
 		for (int j = 0; j < q; j++)
 		{
+			int tmp1, tmp2;
 			cin >> tmp1 >> tmp2;
-			modifications[j] = make_pair(tmp1, tmp2);
+			if (indexs.count(tmp1))
+				indexs.erase(tmp1);
+			if (xorOdd[tmp2])
+				indexs.insert(tmp2);
+			if (indexs.size() % 2 == 0)
+				cout << " " << n;
+			else
+			{
+				int r = *indexs.rbegin();
+				int l = *indexs.begin();
+				cout<<" "<<max(r, n - l - 1);
+			}
 		}
-		auto res = count(str, vecs);
-		cout << "Case #" << i << ": " << res << endl;
+		cout << endl;
 	}
 	return 0;
 }
