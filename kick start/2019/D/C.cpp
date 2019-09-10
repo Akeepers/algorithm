3770aabf1beb7e6cb674acb08b4c3b86906c1f38
Akeepers <578805584@qq.com>
2019-09-10 20:25:46 +0800

    add solution

--- kick start/2019/D/C.cpp
+++ kick start/2019/D/C.cpp
@@ -3,20 +3,84 @@
 #include <climits>
 #include <cmath>
 #include <iostream>
+#include <queue>
 #include <set>
 #include <string>
-#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, int> p;
 
-
-ll solve(ll n,ll k,vector<ll>& positions,vector<ll>& costs)
+typedef struct
 {
-	ll res = 0;
-	
+	ll x, c;
+} Node;
+
+ll solve(ll n, ll k, vector<Node> &positions)
+{
+	ll k1 = k / 2;
+	ll k2 = k - k1;
+	priority_queue<ll> q1;
+	priority_queue<p, vector<p>, greater<>> q2;
+	unordered_set<ll> indexs;
+
+	ll left = 0;
+	for (int i = 0; i < k1; ++i)
+	{
+		auto v = positions[i].c - positions[i].x;
+		left += v;
+		q1.push(v);
+	}
+	for (int i = k1 + 1; i < n; ++i)
+	{
+		q2.push(make_pair(positions[i].c + positions[i].x, i));
+	}
+	ll right = 0;
+
+	for (int i = 0; i < k2; ++i)
+	{
+		auto top = q2.top();
+		right += top.first;
+		indexs.insert(top.second);
+		q2.pop();
+	}
+	ll res = left + right + k1 * positions[k1].x - k2 * positions[k1].x + positions[k1].c;
+	for (int i = k1 + 1; i < n - k2; ++i)
+	{
+		auto cur = positions[i];
+		if (!q1.empty())
+		{
+			auto tmp = cur.c - cur.x;
+			if (tmp < q1.top())
+			{
+				left = left - q1.top() + tmp;
+				q1.pop();
+				q1.push(tmp);
+			}
+		}
+		if (!indexs.empty())
+		{
+			if (indexs.count(i))
+			{
+				int t;
+				do
+				{
+					t = q2.top().second;
+					q2.pop();
+				} while (t <= i && !q2.empty());
+				if (q2.empty())
+					break;
+				indexs.erase(i);
+				indexs.insert(t);
+				right = right - positions[i].c - positions[i].x + positions[t].c + positions[t].x;
+			}
+		}
+		res = min(res, left + right + k1 * cur.x - k2 * cur.x + cur.c);
+	}
+	return res;
 }
 
 int main()
@@ -28,18 +92,18 @@ int main()
 
 		ll k, n;
 		cin >> k >> n;
-		vector<ll> positions(n), costs(n);
+		vector<Node> positions(n);
 		for (ll j = 0; j < n; ++j)
 		{
-			cin >> positions[j];
+			cin >> positions[j].x;
 		}
 		for (ll j = 0; j < n; ++j)
 		{
-			cin >> costs[j];
+			cin >> positions[j].c;
 		}
-		auto res = solve(n,k, positions, costs);
-		cout << "Case #" << i << ":";
-		cout << endl;
+		sort(positions.begin(), positions.end(), [](const Node &a, const Node &b) { return a.x < b.x; });
+		auto res = solve(n, k, positions);
+		cout << "Case #" << i << ": " << res << endl;
 	}
 	return 0;
 }
