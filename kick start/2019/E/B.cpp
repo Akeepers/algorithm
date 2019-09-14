51be5bae5818232862ca03e56d9a504895693abe
Akeepers <578805584@qq.com>
2019-09-14 23:07:34 +0800

    add solution

--- kick start/2019/E/B.cpp
+++ kick start/2019/E/B.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <bitset>
+#include <cfloat>
 #include <climits>
 #include <iostream>
 #include <set>
@@ -10,34 +11,65 @@
 using namespace std;
 
 typedef long long ll;
+const double EPS = 2e-8;
 
-vector<char> slove(vector<pair<ll, ll>> &slots, vector<pair<ll, ll>> &requests, ll d, ll s, ll sumCode, ll sumEate)
+typedef struct
 {
-	vector<char> res(d);
-	sort(slots.begin(), slots.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
-		return (a.first + 1) / (float)(a.second + 1) > (b.first + 1) / (float)(b.second + 1);
-	});
-	float flag = sumCode / (float)sumEate;
-	for (int i = 0; i < d; ++i)
+	ll code, eat, id;
+	char res;
+} Node;
+
+void slove()
+{
+	ll d, s;
+	cin >> d >> s;
+	vector<pair<ll, ll>> slots(s);
+	vector<Node> requests(d);
+	vector<ll> sumCode(s + 1, 0), sumEat(s + 1, 0);
+	for (ll i = 0; i < s; i++)
+		cin >> slots[i].first >> slots[i].second;
+
+	sort(slots.begin(), slots.end(), [](const pair<ll, ll> &a, const pair<ll, ll> &b) { return a.first * b.second > a.second * b.first; });
+
+	for (ll i = 0; i < d; i++)
 	{
-		if (requests[i].first > sumCode || requests[i].second > sumEate)
+		cin >> requests[i].code >> requests[i].eat;
+		requests[i].id = i;
+	}
+
+	sort(requests.begin(), requests.end(), [](const Node &a, const Node &b) { return a.code < b.code; });
+	for (ll i = 1; i <= s; ++i)
+		sumCode[i] = slots[i - 1].first + sumCode[i - 1];
+	for (ll i = s - 1; i >= 0; --i)
+		sumEat[i] = slots[i].second + sumEat[i + 1];
+
+	vector<char> res(d);
+	for (ll i = 0, pos = 0; pos < d; ++pos)
+	{
+		auto &cur = requests[pos];
+		while (i < s - 1 && sumCode[i + 1] < cur.code)
+			i++;
+		if (sumCode[i + 1] >= cur.code && sumEat[i + 1] >= cur.eat)
+			cur.res = 'Y';
+		else
 		{
-			res[i] = 'N';
-		}
-		else if(requests[i].first == 0 )
-		{
-			res[i] = requests[i].second <= sumEate ? 'Y' : 'N';
-		}else if (requests[i].second == 0 )
-		{
-			res[i] = requests[i].first <= sumCode ? 'Y' : 'N';
-		}else if (requests[i].first/(float)requests[i].second>flag){
-			ll cur1 = 0,pos=0;
-			while(cur1<requests[i].first){
-				cur1 += slots[pos++].first;
+			auto code = cur.code - sumCode[i];
+			auto eat = cur.eat - sumEat[i + 1];
+			if (slots[i].first < code || slots[i].second < eat)
+				cur.res = 'N';
+			else if (slots[i].first == 0)
+				cur.res = slots[i].second >= eat ? 'Y' : 'N';
+			else
+			{
+				double f = code / ((double)slots[i].first + EPS);
+				cur.res = (1 - f) * slots[i].second + DBL_MIN >= eat ? 'Y' : 'N';
 			}
-			ll cur2=(cur1-requests[i].first)/(float)requests[i].first
 		}
 	}
+	sort(requests.begin(), requests.end(), [](const Node &a, const Node &b) { return a.id < b.id; });
+
+	for (auto item : requests)
+		cout << item.res;
 }
 int main()
 {
@@ -45,26 +77,8 @@ int main()
 	cin >> t;
 	for (ll i = 1; i <= t; ++i)
 	{
-		cout << "Case #" << i << ":";
-		ll d, s;
-		cin >> d >> s;
-		vector<pair<ll, ll>> slots(s);
-		vector<pair<ll, ll>> requests(d);
-		ll sumCode = 0, sumEate = 0;
-		for (ll j = 0; j < s; j++)
-		{
-			ll x1, x2;
-			cin >> x1 >> x2;
-			sumCode += x1;
-			sumEate += x2;
-			slots[j] = make_pair(x1, x2);
-		}
-		for (ll j = 0; j < d; j++)
-		{
-			ll x1, x2;
-			cin >> x1 >> x2;
-			requests[j] = make_pair(x1, x2);
-		}
+		cout << "Case #" << i << ": ";
+		slove();
 		cout << endl;
 	}
 	return 0;
---
+++ kick start/2019/E/C.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <bitset>
+#include <cfloat>
+#include <climits>
+#include <iostream>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+const double EPS = 2e-8;
+
+void slove()
+{
+}
+int main()
+{
+	auto t = 0;
+	cin >> t;
+	for (ll i = 1; i <= t; ++i)
+	{
+		cout << "Case #" << i << ": ";
+		slove();
+		cout << endl;
+	}
+	return 0;
+}
