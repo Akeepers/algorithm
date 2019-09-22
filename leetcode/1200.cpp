5e593974f9ecde4889fa157a37fd1e6d9863d60e
Akeepers <578805584@qq.com>
2019-09-22 19:28:49 +0800

    update solution

--- leetcode/1200.cpp
+++ leetcode/1200.cpp
@@ -1,32 +1,37 @@
 #include <algorithm>
+#include <bitset>
 #include <climits>
 #include <iostream>
+#include <set>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    char nextGreatestLetter(vector<char> &letters, char target)
-    {
-        int min = INT_MAX;
-        char res;
-        for (auto letter : letters)
-        {
-            int cur = letter > target ? letter - target : 'z' - target + (letter - 'a') + 1;
-            if (cur < min)
-            {
-                min = cur;
-                res = letter;
-            }
-        }
-        return res;
-    }
+	vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+		int diff = INT_MAX,n=arr.size();
+		sort(arr.begin(), arr.end());
+		for(int i=0;i<n-1;++i)
+		{
+			diff = min(diff, arr[i + 1] - arr[i]);
+		}
+		vector<vector<int>> res;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if(arr[i + 1] - arr[i]==diff)
+			{
+				res.emplace_back(vector<int>{arr[i], arr[i + 1]});
+			}
+		}
+		return res;
+	}
 };
+
 
 int main()
 {
-    system("pause");
-    return 0;
+	return 0;
 }
