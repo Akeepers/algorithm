4171a70a0681bfd04cfc29c92a4477bc768c8725
akeeper <578805584@qq.com>
2020-01-31 16:33:25 +0800

    add solution

--- leetcode/1333.cpp
+++ leetcode/1333.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: yangpan
+ * @Date: 2020-01-27 10:16:29
+ * @LastEditors: yangpan
+ * @LastEditTime: 2020-01-31 16:29:50
+ * @Description: file content
+ * @FilePath: \leetcode\1333.cpp
+ */
 #include <algorithm>
 #include <climits>
 #include <iostream>
@@ -6,47 +14,37 @@
 #include <vector>
 
 using namespace std;
-bool mycompare(const vector<int> &a, const vector<int> &b)
-{
-    if (a[1] != b[1])
-    {
+bool mycompare(const vector<int> &a, const vector<int> &b) {
+    if (a[1] != b[1]) {
         return a[1] > b[1];
-    }
-    else
-    {
+    } else {
         return a[0] > b[0];
     }
 }
 
-class Solution
-{
-
-public:
-    vector<int> filterRestaurants(vector<vector<int>> &restaurants, int veganFriendly, int maxPrice, int maxDistance)
-    {
+class Solution {
+   public:
+    vector<int> filterRestaurants(vector<vector<int>> &restaurants,
+                                  int veganFriendly, int maxPrice,
+                                  int maxDistance) {
         vector<vector<int>> temp;
-        for (auto restaurant : restaurants)
-        {
-            if (veganFriendly == 0 || veganFriendly == restaurant[2])
-            {
-                if (restaurant[3] <= maxPrice && restaurant[4] <= maxDistance)
-                {
+        for (auto restaurant : restaurants) {
+            if (veganFriendly == 0 || veganFriendly == restaurant[2]) {
+                if (restaurant[3] <= maxPrice && restaurant[4] <= maxDistance) {
                     temp.push_back(restaurant);
                 }
             }
         }
         sort(temp.begin(), temp.end(), mycompare);
         vector<int> res;
-        for (auto item : temp)
-        {
+        for (auto item : temp) {
             res.push_back(item[0]);
         }
         return res;
     }
 };
 
-int main()
-{
+int main() {
     system("pause");
     return 0;
 }
